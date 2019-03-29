684ab9e3a8227e0dbd9d9a6cfeac9b881dbe2c60
michael murphy <michaelmurphy@salesforce.com>
2019-03-29 10:45:49 +0000

    init

--- cometd-client.js
+++ cometd-client.js
@@ -1,29 +1,24 @@
-// Run the adapter code that implements XMLHttpRequest.
 require('cometd-nodejs-client').adapt();
-
-// Your normal CometD client application here.
 var lib = require('cometd');
 var cometd = new lib.CometD();
+var uri = process.argv[1];
+var channel = process.argv[2];
 
 cometd.configure({
-    // url: 'http://michaelmurphy-eval-test.apigee.net/subtostream3/cometd:80'
-    url: 'http://localhost/cometd:9000'
+    url: uri,
+    appendMessageTypeToURL: false
 });
-// cometd.addListener('/event/Test__e', function(message) {
-//     console.dir(message);
-// });
 
 
 
 cometd.handshake(function(handshakeReply) {
 
     if (handshakeReply.successful) {
-        // Your logic here.
         console.log('handshake');
         console.dir(handshakeReply);
 
         
-        cometd.subscribe('/event/Test__e', function(message) {
+        cometd.subscribe(channel, function(message) {
             console.dir(message);
         }, function(subscribeReply) {
             console.log('subscribed');
